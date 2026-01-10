9599d7f51cbcc79517ba15a3c7745664c6e71918
amanTesfa <amantesfa1010@gmail.com>
2026-01-10 23:06:02 +0300

    initial commit

---
+++ progress-front/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className='box' id='box1'>
+        Percentage 1
+        </div>
+        <div className='box' id='box2'>
+         Percentage 2
+        </div>
+        <div className='box' id='box3'>
+         Percentage 3
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
